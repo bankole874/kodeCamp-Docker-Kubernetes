3fe7c1c12a7a7ec9e1f45754e5f4624b8769fcdd
bankole874 <damilarehammed.hd@gmail.com>
2024-08-01 10:19:48 +0100

    second commit

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Welcome to KodeCamp DevOps Bootcamp!"
+    message = os.getenv('WELCOME_MESSAGE', "Hello, Welcome to KodeCamp DevOps Bootcamp!")
+    return message
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
